a71a68a9f512265b29bc907a0eb0c3217007dddf
HRPzz <royalpkgh@naver.com>
2022-09-13 03:50:29 +0000

    todo template
    HTML 코딩 하기

--- vue_django_jquery/02_DJTodo/todo/models.py
+++ vue_django_jquery/02_DJTodo/todo/models.py
@@ -7,3 +7,12 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.todo
+
+    # save 메서드 오버라이딩
+    def save(self, force_insert=False, force_update=False, using=None,
+            update_fileds=None):
+        # 이름 기본값 설정
+        if not self.name:
+            self.name = '홍길동'
+        # DB 에 레코드 저장
+        super().save()
