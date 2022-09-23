46bd841d57195694538c93cb0d32c5fe75e4d7f2
HRPzz <royalpkgh@naver.com>
2022-09-23 12:38:17 +0000

    todo index url
    todo 앱 기본 코딩 하기

--- vue_django_jquery/03_VueDjTodo/todo/models.py
+++ vue_django_jquery/03_VueDjTodo/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    name = models.CharField('NAME', max_length=5, blank=True)
+    todo = models.CharField('TODO', max_length=50)
+
+    def __str__(self):
+        return self.todo
+
+    # save 메서드 오버라이딩
+    def save(self, force_insert=False, force_update=False, using=None,
+            update_fileds=None):
+        # 이름 기본값 설정
+        if not self.name:
+            self.name = '홍길동'
+        # DB 에 레코드 저장
+        super().save()
